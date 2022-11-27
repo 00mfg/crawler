4c9b45495e3abec6f63288943a19d5c4def74f6e
lc <tron.liu@gmail.com>
2022-11-27 23:39:05 +0800

    lesson19

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/00mfg/crawler
 
 go 1.17
+
+require (
+	github.com/antchfx/htmlquery v1.2.5
+	golang.org/x/net v0.2.0
+	golang.org/x/text v0.4.0
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/antchfx/xpath v1.2.1 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+)
--- main.go
+++ main.go
@@ -1,30 +1,102 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/antchfx/htmlquery"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
 )
 
-func main()  {
+var headerRe = regexp.MustCompile(`<div class="small_cardcontent__BTALp"[\s\S]*?<h2>([\s\S]*?)</h2>`)
+
+func main() {
 	url := "https://www.thepaper.cn/"
-	resp, err:= http.Get(url)
-	if err!= nil {
-		fmt.Printf("fetch url err:%v\n", err)
+
+	body, err := Fetch(url)
+
+	if err != nil {
+		fmt.Printf("read content failed %v", err)
 		return
+	}
+
+	//res := REParse(body)
+	//res := XpathParse(body)
+	res := CSSParse(body)
+	fmt.Println(res)
+
+}
+
+func Fetch(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 		fmt.Printf("Error status code: %v", resp.StatusCode)
-		return
 	}
-	body, err := ioutil.ReadAll(resp.Body)
+	bodyReader := bufio.NewReader(resp.Body)
+	e := DerterminEncoding(bodyReader) //utf-8
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func DerterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
 
 	if err != nil {
-		fmt.Printf(" read content failer: %v", err)
-		return
+		fmt.Printf("fetch error: %v", err)
+		return unicode.UTF8
 	}
 
-	fmt.Println("body", string(body))
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
+
+func REParse(page []byte) []string {
+	var res []string
+	matches := headerRe.FindAllSubmatch(page, -1)
+
+	for _, m := range matches {
+		res = append(res, string(m[1]))
+	}
+	return res
+}
+
+func XpathParse(body []byte) []string {
+	var res []string
+	doc, err := htmlquery.Parse(bytes.NewReader(body))
+	if err != nil {
+		fmt.Printf(" htmlquery Parse failed: %v", err)
+	}
+
+	nodes := htmlquery.Find(doc, `//div[@class="small_toplink__GmZhY"]/a/h2`)
+
+	for _, node := range nodes {
+		res = append(res, node.FirstChild.Data)
+	}
+	return res
+}
+
+func CSSParse(body []byte) []string {
+	var res []string
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(body))
+	if err != nil {
+		fmt.Printf("read content failed: %v", err)
+	}
+	doc.Find("div a h2").Each(func(i int, s *goquery.Selection) {
+		title := s.Text()
+		res = append(res, title)
+	})
+	return res
 }
